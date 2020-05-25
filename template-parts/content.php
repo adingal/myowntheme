6f32aa1608b8871af0eb97062a7c55ef53669456
Alex Dingal <lhexie2014@gmail.com>
2020-05-26 00:41:19 +0800

    Add conditional check if single or index page

--- template-parts/content.php
+++ template-parts/content.php
@@ -35,27 +35,35 @@
 
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'myowntheme' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'myowntheme' ),
-				'after'  => '</div>',
-			)
-		);
+		if ( is_singular() ) :
+			the_content(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'myowntheme' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
+
+			wp_link_pages(
+				array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'myowntheme' ),
+					'after'  => '</div>',
+				)
+			);
+		else :
+
+			the_excerpt();
+
+		endif;
+		
 		?>
 	</div><!-- .entry-content -->
 	

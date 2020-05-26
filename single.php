34d482d0bc78aad53baeab08fcc53979da301e4b
Alex Dingal <lhexie2014@gmail.com>
2020-05-27 00:52:24 +0800

    Change single post layout

--- single.php
+++ single.php
@@ -11,29 +11,31 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-		<div class="post-content">
-			<?php
-			while ( have_posts() ) :
-				the_post();
-
-				get_template_part( 'template-parts/content', 'single' );
-
-				the_post_navigation(
-					array(
-						'prev_text' => '<span class="nav-title">%title</span>',
-						'next_text' => '<span class="nav-title">%title</span>',
-					)
-				);
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-		</div>
-		<?php get_sidebar(); ?>
+		<div class="post-wrapper">
+			<div class="post-content">
+				<?php
+				while ( have_posts() ) :
+					the_post();
+	
+					get_template_part( 'template-parts/content', 'single' );
+	
+					the_post_navigation(
+						array(
+							'prev_text' => '<span class="nav-title">%title</span>',
+							'next_text' => '<span class="nav-title">%title</span>',
+						)
+					);
+	
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+	
+				endwhile; // End of the loop.
+				?>
+			</div><!-- .post-content -->
+			<?php get_sidebar(); ?>
+		</div><!-- .post-wrapper -->
 	</main><!-- #main -->
 
 <?php

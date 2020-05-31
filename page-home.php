118416451cc5d9f0edfd440e1970022458a90b5a
Alex Dingal <lhexie2014@gmail.com>
2020-06-01 01:58:24 +0800

    Add initial custom fields and post types to home page template

--- page-home.php
+++ page-home.php
@@ -3,10 +3,54 @@
  * Template Name: Homepage
  */
 
+// Get fields
+$welcome_title = get_field( 'welcome_title' );
+$welcome_description = get_field( 'welcome_description' );
+
 get_header();
 ?>
 
+	<main id="primary" class="site-main">
+        <div class="welcome">
+            <div class="welcome-text">
+                <h1><?php echo esc_html__( $welcome_title, 'myowntheme' ); ?></h1>
+                <p><?php echo esc_html__( $welcome_description, 'myowntheme' ); ?></p>
+            </div>
+            <div class="technologies">
+                <div class="languages">
+                    <?php
+                        $args = array(
+                            'post_type' => 'technologies',
+                            'posts_per_page' => 6,
+                            'order' => 'ASC',
+                            'orderby' => 'ID',
+                        );
+                        $technologies = new WP_Query ( $args );
 
+                        if ( $technologies->have_posts() ) :
+                            while ( $technologies->have_posts() ) :
+                                $technologies->the_post();
+                    ?>
+
+                    <?php
+                        if ( has_post_thumbnail() ) :
+                            the_post_thumbnail();
+                        endif;
+                    ?>
+
+                    <?php
+                            endwhile;
+                        endif;
+                    ?>
+                </div>
+                <div class="tools">
+                    <img src="" alt="Git logo">
+                    <img src="" alt="Gulp logo">
+                    <img src="" alt="NPM logo">
+                </div>
+            </div>
+        </div>
+	</main><!-- #main -->
 
 <?php
 get_footer();

9db6e9b41f222563f83c97d11d740ed97dc82401
Alex Dingal <lhexie2014@gmail.com>
2020-06-01 20:02:32 +0800

    Create homepage welcome section, layout and design

--- page-home.php
+++ page-home.php
@@ -10,47 +10,73 @@ $welcome_description = get_field( 'welcome_description' );
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-        <div class="welcome">
-            <div class="welcome-text">
-                <h1><?php echo esc_html__( $welcome_title, 'myowntheme' ); ?></h1>
-                <p><?php echo esc_html__( $welcome_description, 'myowntheme' ); ?></p>
-            </div>
-            <div class="technologies">
-                <div class="languages">
-                    <?php
-                        $args = array(
-                            'post_type' => 'technologies',
-                            'posts_per_page' => 6,
-                            'order' => 'ASC',
-                            'orderby' => 'ID',
-                        );
-                        $technologies = new WP_Query ( $args );
-
-                        if ( $technologies->have_posts() ) :
-                            while ( $technologies->have_posts() ) :
-                                $technologies->the_post();
-                    ?>
-
-                    <?php
-                        if ( has_post_thumbnail() ) :
-                            the_post_thumbnail( 'welcome-image' );
-                        endif;
-                    ?>
-
-                    <?php
-                            endwhile;
-                        endif;
-                        wp_reset_postdata();
-                    ?>
+	<main id="primary">
+        <section class="welcome" data-type="background" data-speed="4">
+            <div class="backdrop">
+                <div class="welcome-text">
+                    <h1><?php echo esc_html__( $welcome_title, 'myowntheme' ); ?></h1>
+                    <p><?php echo esc_html__( $welcome_description, 'myowntheme' ); ?></p>
                 </div>
-                <div class="tools">
-                    <img src="" alt="Git logo">
-                    <img src="" alt="Gulp logo">
-                    <img src="" alt="NPM logo">
-                </div>
-            </div>
-        </div>
+                <div class="technologies">
+                    <p>Tools &amp; technologies:</p>
+                    <div class="languages">
+                        <?php
+                            $args = array(
+                                'post_type' => 'technologies',
+                                'posts_per_page' => 6,
+                                'order' => 'ASC',
+                                'orderby' => 'ID',
+                            );
+                            $technologies = new WP_Query ( $args );
+    
+                            if ( $technologies->have_posts() ) :
+                                while ( $technologies->have_posts() ) :
+                                    $technologies->the_post();
+                        ?>
+    
+                        <?php
+                            if ( has_post_thumbnail() ) :
+                                the_post_thumbnail();
+                            endif;
+                        ?>
+    
+                        <?php
+                                endwhile;
+                            endif;
+                            wp_reset_postdata();
+                        ?>
+                    </div><!-- .languages -->
+                    <div class="tools">
+                        
+                        <?php
+                            $args = array(
+                                'post_type' => 'web_tools',
+                                'posts_per_page' => 6,
+                                'order' => 'ASC',
+                                'orderby' => 'title',
+                            );
+                            $web_tools = new WP_Query ( $args );
+    
+                            if ( $web_tools->have_posts() ) :
+                                while ( $web_tools->have_posts() ) :
+                                    $web_tools->the_post();
+                        ?>
+    
+                        <?php
+                            if ( has_post_thumbnail() ) :
+                                the_post_thumbnail();
+                            endif;
+                        ?>
+    
+                        <?php
+                                endwhile;
+                            endif;
+                            wp_reset_postdata();
+                        ?>
+                    </div><!-- .tools -->
+                </div><!-- .technologies -->
+            </div><!-- .backdrop -->
+        </section><!-- .welcome -->
 	</main><!-- #main -->
 
 <?php

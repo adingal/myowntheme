b03f7ec19e43a2feff2cf54cd66d168f6cc7c06b
Alex Dingal <lhexie2014@gmail.com>
2020-06-04 01:40:02 +0800

    Change search page default title

--- search.php
+++ search.php
@@ -13,7 +13,7 @@ get_header();
 		<h1 class="page-title">
 			<?php
 			/* translators: %s: search query. */
-			printf( esc_html__( 'Search Results for: %s', 'myowntheme' ), '<span>' . get_search_query() . '</span>' );
+			printf( esc_html__( '%s', 'myowntheme' ), '<span>' . get_search_query() . '</span>' );
 			?>
 		</h1>
 	</section><!-- .page-header -->

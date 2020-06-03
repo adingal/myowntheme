f417d63d9aa1a1f4581a34a4e69b7209e0e14588
Alex Dingal <lhexie2014@gmail.com>
2020-06-04 01:34:25 +0800

    Modify 404 page design

--- 404.php
+++ 404.php
@@ -10,16 +10,18 @@
 get_header();
 ?>
 	<section class="page-header" data-type="background" data-speed="4">
-		<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'myowntheme' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e( 'Oops! Page not found.', 'myowntheme' ); ?></h1>
 	</section><!-- .page-header -->
 
 	<main id="primary" class="site-main">
 
 		<section class="error-404 not-found">
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found. Try one of the links below or search again?', 'myowntheme' ); ?></p>
-
-				<?php get_search_form(); ?>
+				<div class="page-content-body">
+					<p><?php esc_html_e( 'It looks like nothing was found. Try one of the links below or search again?', 'myowntheme' ); ?></p>
+	
+					<?php get_search_form(); ?>
+				</div>
 
 				<?php get_template_part( 'template-parts/content', 'recent_posts' ); ?>
 

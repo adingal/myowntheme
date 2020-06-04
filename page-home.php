a9362d568938cc1781a29e89dfec18f657353181
Alex Dingal <lhexie2014@gmail.com>
2020-06-05 04:41:57 +0800

    Add custom post type for projects

--- page-home.php
+++ page-home.php
@@ -13,101 +13,130 @@ $project_intro_body = get_field( 'project_introduction_body' );
 get_header();
 ?>
 
-	<main id="primary">
-        <section class="welcome" data-type="background" data-speed="2">
-            <div class="backdrop">
-                <div class="welcome-text">
-                    <h1><?php echo esc_html__( $welcome_title, 'myowntheme' ); ?></h1>
-                    <p><?php echo esc_html__( $welcome_body, 'myowntheme' ); ?></p>
-                </div>
-                <div class="technologies">
-                    <p>Tools &amp; technologies:</p>
-                    <div class="languages">
-                        <?php
-                            $args = array(
-                                'post_type' => 'technologies',
-                                'posts_per_page' => 10,
-                                'order' => 'ASC',
-                                'orderby' => 'ID',
-                            );
-                            $technologies = new WP_Query ( $args );
-    
-                            if ( $technologies->have_posts() ) :
-                                while ( $technologies->have_posts() ) :
-                                    $technologies->the_post();
-                        ?>
-    
-                        <?php
-                            if ( has_post_thumbnail() ) :
-                                the_post_thumbnail();
-                            endif;
-                        ?>
-    
-                        <?php
-                                endwhile;
-                            endif;
-                            wp_reset_postdata();
-                        ?>
-                    </div><!-- .languages -->
-                    <div class="tools">
-                        
-                        <?php
-                            $args = array(
-                                'post_type' => 'web_tools',
-                                'posts_per_page' => 6,
-                                'order' => 'ASC',
-                                'orderby' => 'title',
-                            );
-                            $web_tools = new WP_Query ( $args );
-    
-                            if ( $web_tools->have_posts() ) :
-                                while ( $web_tools->have_posts() ) :
-                                    $web_tools->the_post();
-                        ?>
-    
-                        <?php
-                            if ( has_post_thumbnail() ) :
-                                the_post_thumbnail();
-                            endif;
-                        ?>
-    
-                        <?php
-                                endwhile;
-                            endif;
-                            wp_reset_postdata();
-                        ?>
-                    </div><!-- .tools -->
-                </div><!-- .technologies -->
-            </div><!-- .backdrop -->
-        </section><!-- .welcome -->
+<main id="primary">
+    <section class="welcome" data-type="background" data-speed="2">
+        <div class="backdrop">
+            <div class="welcome-text">
+                <h1><?php echo esc_html__( $welcome_title, 'myowntheme' ); ?></h1>
+                <p><?php echo esc_html__( $welcome_body, 'myowntheme' ); ?></p>
+            </div>
+            <div class="technologies">
+                <p>Tools &amp; technologies:</p>
+                <div class="languages">
+                    <?php
+                        $args = array(
+                            'post_type' => 'technologies',
+                            'posts_per_page' => 10,
+                            'order' => 'ASC',
+                            'orderby' => 'ID',
+                        );
+                        $technologies = new WP_Query ( $args );
 
-        <div class="project-intro">
-            <h2>
-                <?php
-                    if ( !empty( $project_intro_title ) ) :
-                        echo esc_html__( $project_intro_title, 'myowntheme' );
-                    else :
-                        echo esc_html__( 'Project Title', 'myowntheme' );
-                    endif;
-                ?>
-            </h2>
-            <p>
-                <?php
-                    if ( !empty( $project_intro_body ) ) :
-                        echo esc_html__( $project_intro_body, 'myowntheme' );
-                    else :
-                        echo esc_html__( 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, perspiciatis eligendi architecto ullam dicta.', 'myowntheme' );
-                    endif;
-                ?>
-            </p>
-        </div><!-- .project-intro -->
+                        if ( $technologies->have_posts() ) :
+                            while ( $technologies->have_posts() ) :
+                                $technologies->the_post();
+                    ?>
+
+                    <?php
+                        if ( has_post_thumbnail() ) :
+                            the_post_thumbnail();
+                        endif;
+                    ?>
+
+                    <?php
+                            endwhile;
+                        endif;
+                        wp_reset_postdata();
+                    ?>
+                </div><!-- .languages -->
+                <div class="tools">
+                    
+                    <?php
+                        $args = array(
+                            'post_type' => 'web_tools',
+                            'posts_per_page' => 6,
+                            'order' => 'ASC',
+                            'orderby' => 'title',
+                        );
+                        $web_tools = new WP_Query ( $args );
+
+                        if ( $web_tools->have_posts() ) :
+                            while ( $web_tools->have_posts() ) :
+                                $web_tools->the_post();
+                    ?>
+
+                    <?php
+                        if ( has_post_thumbnail() ) :
+                            the_post_thumbnail();
+                        endif;
+                    ?>
+
+                    <?php
+                            endwhile;
+                        endif;
+                        wp_reset_postdata();
+                    ?>
+                </div><!-- .tools -->
+            </div><!-- .technologies -->
+        </div><!-- .backdrop -->
+    </section><!-- .welcome -->
+
+    <div class="project-intro">
+        <h2>
+            <?php
+                if ( !empty( $project_intro_title ) ) :
+                    echo esc_html__( $project_intro_title, 'myowntheme' );
+                else :
+                    echo esc_html__( 'Project Title', 'myowntheme' );
+                endif;
+            ?>
+        </h2>
+        <p>
+            <?php
+                if ( !empty( $project_intro_body ) ) :
+                    echo esc_html__( $project_intro_body, 'myowntheme' );
+                else :
+                    echo esc_html__( 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, perspiciatis eligendi architecto ullam dicta.', 'myowntheme' );
+                endif;
+            ?>
+        </p>
+    </div><!-- .project-intro -->
+
+    <?php
+        $args = array(
+            'post_type' => 'projects',
+            'posts_per_page' => 6,
+            'order' => 'ASC',
+            'orderby' => 'ID',
+        );
+        $projects = new WP_Query ( $args );
+
+        if ( $projects->have_posts() ) :
+            while ( $projects->have_posts() ) :
+                $projects->the_post();
+    ?>
+        
         <section class="projects">
-            <div class="project-item"></div>
-            <div class="project-item"></div>
-            <div class="project-item"></div>
-            <div class="project-item"></div>
-        </section>
-	</main><!-- #main -->
+            <div class="project-content">
+                <h3><?php the_title(); ?></h3>
+                <p><?php the_content(); ?></p>
+                <a href="<?php the_permalink(); ?>" class="continue-reading">Check it out</a>
+            </div>
+            <div class="project-highlights">
+                <?php
+                    if ( has_post_thumbnail() ) :
+                        the_post_thumbnail();
+                    endif;
+                ?>
+            </div>
+        </section><!-- .projects -->  
+
+    <?php
+            endwhile;
+        endif;
+        wp_reset_postdata();
+    ?>
+</main><!-- #main -->
 
 <?php
 get_footer();
